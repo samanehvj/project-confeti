899144ceeb53f1665ffdba9265bfe14a84563aa2
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2023-06-26 09:11:11 -0700

    Create 3col_contentContainer.php

---
+++ views/3col_contentContainer.php
@@ -0,0 +1,10 @@
+<div class="container col-12 col-lg-12" id="grade">
+
+    <!-- put contents here -->
+    <?=$this->rightHTML?>
+    <!-- put contents here -->
+    <?=$this->leftHTML?>
+    <!-- put contents here -->
+    <?=$this->centerHTML?>		
+		
+</div><!-- .container / dashboardContainer.php -->
